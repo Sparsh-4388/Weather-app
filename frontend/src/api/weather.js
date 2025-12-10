2176c88128a8dc659e85f7cb4d4344f8a2deb730
Sparsh <sparshp773@gmail.com>
2025-12-10 17:09:06 +0530

    Updated URL

--- frontend/src/api/weather.js
+++ frontend/src/api/weather.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/weather"; // backend endpoint
+const BASE_URL = "https://weather-app-51f0.onrender.com/"; // backend endpoint
 
 export async function getWeatherByCity(city) {
   const res = await axios.get(BASE_URL, { params: { city } });
